1b6dbf6420b66b049b871a32dbbf192a5a2cc0dc
tommypacker <tommypacker@gmail.com>
2018-01-05 11:33:23 -0800

    Redownload songs if they don't exist locally

--- celery_tasks.py
+++ celery_tasks.py
@@ -2,8 +2,9 @@ import youtube_dl
 import downloader as dl
 from celery import Celery
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 from models import Song
-
+from pathlib import Path
 
 CELERY_NAME = 'concert'
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
@@ -35,18 +36,19 @@ def async_download(url):
 		for entry in info['entries']:
 			queried_song = db.Downloaded.find_one({'url': entry['webpage_url']})
 			if queried_song != None:
-				new_song = Song(queried_song['mrl'], queried_song['title'], queried_song['url'])
-				db.Queue.insert_one(new_song.dictify())
-				continue
+				if _file_exists(queried_song['mrl']):
+					new_song = Song(queried_song['mrl'], queried_song['title'], queried_song['url'])
+					db.Queue.insert_one(new_song.dictify())
+					continue
+				else:
+					db.Downloaded.delete_one({'_id': ObjectId(queried_song['_id'])})
 
 			info = ytdl.extract_info(entry['webpage_url'], download=True)
-
 			song_id = info["id"]
 			song_title = info["title"]
 
 			# This is jank for now
 			song_mrl = "music/" + str(song_id) + ".mp3"
-
 			new_song = Song(song_mrl, song_title, info['webpage_url'])
 			db.Queue.insert_one(new_song.dictify())
 			db.Downloaded.insert_one(new_song.dictify())
@@ -54,21 +56,26 @@ def async_download(url):
 		# If not a playlist assume single song
 		queried_song = db.Downloaded.find_one({'url': url})
 		if queried_song != None:
-			new_song = Song(queried_song['mrl'], queried_song['title'], queried_song['url'])
-			db.Queue.insert_one(new_song.dictify())
-			return
+			if _file_exists(queried_song['mrl']):
+				new_song = Song(queried_song['mrl'], queried_song['title'], queried_song['url'])
+				db.Queue.insert_one(new_song.dictify())
+				return
+			else:
+				db.Downloaded.delete_one({'_id': ObjectId(queried_song['_id'])})
 
 		info = ytdl.extract_info(url, download=True)
-
 		song_id = info["id"]
 		song_title = info["title"]
 
 		# This is jank for now
 		song_mrl = "music/" + str(song_id) + ".mp3"
-
 		new_song = Song(song_mrl, song_title, url)
 		db.Queue.insert_one(new_song.dictify())
 		db.Downloaded.insert_one(new_song.dictify())
+
+def _file_exists(mrl):
+	file = Path(mrl)
+	return file.is_file()
 	
 if __name__ == '__main__':
     celery.start()
