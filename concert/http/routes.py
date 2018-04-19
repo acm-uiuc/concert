472408cbb515959772448186730196bc8a670950
Naren Dasan <naren@narendasan.com>
2018-04-19 17:47:21 -0500

    apple stuff

--- concert/http/routes.py
+++ concert/http/routes.py
@@ -18,7 +18,7 @@ def index():
 
 @ConcertRESTRoutes.route('/apple-app-site-association')
 def apple():
-    return send_from_directory(os.getcwd() + "/static/ios-config/apple-app-site-association")
+    return send_from_directory(os.getcwd() + "/static/ios-config", "apple-app-site-association")
 
 @ConcertRESTRoutes.route('/static/<path:path>')
 def serve_static_files(path):
