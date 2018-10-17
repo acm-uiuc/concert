fc26a0374eff8bd91dc9f6eb98e00d5d8b368840
asherdale <asher@dales.org>
2018-10-16 19:47:51 -0500

    General html and js refactoring

--- static/js/app.js
+++ static/js/app.js
@@ -1,29 +1,30 @@
-audioState = {
+const audioState = {
     song: null,
-    id: null, 
+    id: null,
     time: 0,
     endTime: 0,
     progressInterval: null,
     playedby: null
 };
-windowUI = {
+
+const windowUI = {
     body: $('body'),
     loginBtn: $('#submit-btn'),
     importBtn: $('#import-btn'),
     searchBox: $('#url-textbox'),
     spinner: $('.spinner'),
-    acm_logo: $('#acm-logo'),
-    title_text: $('.title-text'),
+    acmLogo: $('#acm-logo'),
+    titleText: $('.title-text'),
     button: $('.button'),
-    import_btn: $('#import-btn'),
-    search_bar: $(".select2-container"),
-    search_text: $(".select2-search__field"),
+    searchBar: $(".select2-container"),
+    searchText: $(".select2-search__field"),
     playlist: $(".playlist"),
-    bg_color: null,
-    fg_color: null,
-    other_styles: null
+    bgColor: null,
+    fgColor: null,
+    otherStyles: null
 };
-playerUI = {
+
+const playerUI = {
     thumbnail: null,
     playerMain: $('#main'),
     playerTitle: $('#title'),
@@ -33,76 +34,77 @@ playerUI = {
     playerContent: $('#player-content'),
     clearBtn: $('#clear')
 };
-var loginModal = $('#login-modal');
-var queue = $('#playlist');
+
+const loginModal = $('#login-modal');
+const queue = $('#playlist');
 
 
 /* Setup Desktop Notifcations */
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
     if (!Notification) {
-      alert('Desktop notifications not available in your browser. Try Firefox.'); 
-      return;
+        alert('Desktop notifications not available in your browser. Try Firefox.');
+    } else if (Notification.permission !== "granted") {
+        Notification.requestPermission();
     }
-    if (Notification.permission !== "granted") {
-      Notification.requestPermission();
-    }
-  });
-  
-  function notifyPlayed() {
-      if (Notification.permission !== "granted")
-          Notification.requestPermission();
-      else {
-          var notification = new Notification(`Now Playing ${audioState.song}`, {
-            icon: playerUI.thumbnail,
-            body: `Queued by ${audioState.playedby}`,
-          });
-          notification.onshow = function(event) { 
-              setTimeout(function() { notification.close(); }, 3000);
-          }
-      }
-  }
-
-// Setup queue menu button
-$('.menu').click(function() {
-  playerUI.playerContent.toggleClass('show');
-  playerUI.clearBtn.toggleClass('clear-queue-hidden');
 });
 
-$(document).ready(function() {
-    var sheet = document.createElement('style');
+function notifyPlayed() {
+    if (Notification.permission !== "granted")
+        Notification.requestPermission();
+    else {
+        const notification = new Notification(`Now Playing ${audioState.song}`, {
+            icon: playerUI.thumbnail,
+            body: `Queued by ${audioState.playedby}`,
+        });
+        notification.onshow = (event) => {
+            setTimeout(notification.close, 3000);
+        }
+    }
+}
+
+// Setup queue menu button
+$('.menu').click(() => {
+    playerUI.playerContent.toggleClass('show');
+    playerUI.clearBtn.toggleClass('clear-queue-hidden');
+});
+
+$(document).ready(() => {
+    const sheet = document.createElement('style');
     document.head.appendChild(sheet);
-    windowUI.other_styles = sheet.sheet;
+    windowUI.otherStyles = sheet.sheet;
 });
 
 /* Document key detection */
-$(document).keyup(function(e) {
-    if (e.keyCode == 13) {
-        if (loginModal.css('display') == "block") {
-            windowUI.loginBtn.click();
-        } else {
-            if (!searchJustActive || userJustClicked) {
-                windowUI.importBtn.click();
-            } else {
-                searchJustActive = false;
-            }
-        }
-    }
-    if (e.keyCode == 27) {
-        if (loginModal.css('display') == "block") {
-            loginModal.css('display', "none");
-        }
-    }
-    if (e.keyCode == 32 && !searchCurrentlyActive) {
+$(document).keyup((event) => {
+    const keyCodeIs13 = event.keyCode == 13;
+    
+    if (keyCodeIs13 && loginModal.css('display') == "block") {
+        windowUI.loginBtn.click();
+    } else if (keyCodeIs13 && (!searchJustActive || userJustClicked)) {
+        windowUI.importBtn.click();
+    } else if (keyCodeIs13) {
+        searchJustActive = false;
+    } else if (event.keyCode == 27 && loginModal.css('display') == "block") {
+        loginModal.css('display', "none");
+    } else if (event.keyCode == 32 && !searchCurrentlyActive) {
         socket.emit('c_pause');
     }
+
     userJustClicked = false;
 });
 
+$(".select2-search__field").keyup((event) => {
+    event.preventDefault();
+
+    if (event.keyCode === 13) {
+        windowUI.importBtn.click();
+    }
+});
+
 /* Login Functions */
-windowUI.loginBtn.click(function () {
-    var username = $('#uname-input').val();
-    var password = $('#password-input').val();
-    var data = {"username": username, "password": password};
+windowUI.loginBtn.click(() => {
+    const username = $('#uname-input').val();
+    const password = $('#password-input').val();
 
     windowUI.loginBtn.css("display", "none");
     windowUI.spinner.css("display", "block");
@@ -111,51 +113,56 @@ windowUI.loginBtn.click(function () {
     $.ajax({
         url: '/login',
         type: "POST",
-        data: JSON.stringify(data),
+        data: JSON.stringify({username, password}),
         contentType: "application/json",
-      }).done(function (response) {
+    }).done((response) => {
         window.location.replace("/");
-      }).fail(function (response) {
+    }).fail((response) => {
         alert(response.responseText);
         windowUI.loginBtn.html('Login');
         windowUI.loginBtn.css("display", "block");
         windowUI.spinner.css("display", "none");
         windowUI.loginBtn.css('width', '80px');
         windowUI.loginBtn.removeAttr("disabled");
-      });
+    });
 });
 
 /* Queue Functions */
 function createQueueItem(title, time, id, playedby, songPlaying) {
-    time = formatSeconds(time/1000);
+    time = formatSeconds(time / 1000);
 
-    var entry = document.createElement('li');
+    let entry = document.createElement('li');
+
     if (songPlaying) {
         entry.className += "playing";
     } else {
         entry.className += "playlist-item";
         entry.setAttribute("onclick", "playlistAction(this)");
     }
-    entry.setAttribute("onmouseover", "viewWhoPlayed(this)");
-    entry.setAttribute("onmouseout", "hideWhoPlayed(this)");
+
+    entry.setAttribute("onmouseover", "showTrackAttribute(this, 'playedby')");
+    entry.setAttribute("onmouseout", "showTrackAttribute(this, 'title')");
+
     entry.dataset.title = title;
     entry.dataset.id = id;
     entry.dataset.time = time;
     entry.dataset.playedby = playedby;
 
-    var content = document.createElement('a');
+    const content = document.createElement('a');
     content.className += "track";
     content.innerText = title;
 
-    var timeInfo = document.createElement('span');
+    const timeInfo = document.createElement('span');
     timeInfo.className += "time";
     timeInfo.innerText = time;
 
-    var playlistClearSpan = document.createElement('span');
+    const playlistClearSpan = document.createElement('span');
     playlistClearSpan.style.display = "none";
     playlistClearSpan.className += "time";
-    var itemClearBtn = document.createElement('i');
+
+    const itemClearBtn = document.createElement('i');
     itemClearBtn.className += "fa fa-times item-clear";
+
     playlistClearSpan.appendChild(itemClearBtn);
 
     entry.appendChild(content);
@@ -164,83 +171,79 @@ function createQueueItem(title, time, id, playedby, songPlaying) {
     return entry;
 }
 
-function reloadQueue(serverQueue){
-    if (audioState.song != null) {
-        var firstSong = createQueueItem(audioState.song, audioState.endTime, audioState.id, audioState.playedby, true);
-        var len = serverQueue.length;
-        queue.empty();
-        queue.append(firstSong);
-        for(var i = 0; i < len; i++){
-            var curSong = serverQueue[i];
-            var newQueuedSong = createQueueItem(curSong.title, curSong.duration, curSong.id, curSong.playedby, false);
-            queue.append(newQueuedSong);
-        }
+function reloadQueue(serverQueue) {
+    if (audioState.song == null) {
+        return;
     }
+
+    const firstSong = createQueueItem(audioState.song, audioState.endTime, audioState.id, audioState.playedby, true);
+
+    queue.empty();
+    queue.append(firstSong);
+
+    serverQueue.forEach((song) => {
+        const newQueuedSong = createQueueItem(song.title, song.duration, song.id, song.playedby, false);
+        queue.append(newQueuedSong);
+    });
 }
 
-function viewWhoPlayed(obj) {
-    var titleHolder = $($(obj).children()[0]);
-    titleHolder.text(obj.dataset.playedby);
-}
-
-function hideWhoPlayed(obj) {
-    var titleHolder = $($(obj).children()[0]);
-    titleHolder.text(obj.dataset.title);
+function showTrackAttribute(obj, attr) {
+    const titleHolder = $($(obj).children()[0]);
+    titleHolder.text(obj.dataset[attr]);
 }
 
 function playlistAction(obj) {
-    //var timeHolder = $($(obj).children()[1]);
-    //var clearHolder = $($(obj).children()[2]);
-    //timeHolder.toggle();
-    //clearHolder.toggle();
     if (confirm("Clear From Queue?")) {
         socket.emit('c_remove_song', obj.dataset.id);
     }
 }
 
 function get_handlers() {
-    windowUI.search_bar = $(".select2-container");
-    windowUI.search_text = $(".select2-search__field");
+    windowUI.searchBar = $(".select2-container");
+    windowUI.searchText = $(".select2-search__field");
     windowUI.search_selection = $(".select2-selection__choice");
 }
 
-function color_style(button_color, bg_color) {
-    windowUI.body.css('background-color', bg_color);
-    windowUI.acm_logo.css("fill", button_color);
-    windowUI.title_text.css('color', button_color);
+function color_style(button_color, bgColor) {
+    windowUI.body.css('background-color', bgColor);
+    windowUI.acmLogo.css("fill", button_color);
+    windowUI.titleText.css('color', button_color);
     windowUI.button.css('background-color', button_color);
-    windowUI.button.css('color', bg_color);
-    windowUI.import_btn.css("background-color", button_color);
-    windowUI.import_btn.css("color", bg_color);
-    windowUI.search_bar.attr('style',`border-bottom-color:${button_color}; color:${button_color};`);
-    windowUI.search_text.attr('style',`width:58vw; color:${button_color};`);
-    if (windowUI.other_styles.cssRules.length > 0) {
-        windowUI.other_styles.deleteRule(1);
-        windowUI.other_styles.deleteRule(0);
+    windowUI.button.css('color', bgColor);
+    windowUI.importBtn.css("background-color", button_color);
+    windowUI.importBtn.css("color", bgColor);
+    windowUI.searchBar.attr('style', `border-bottom-color:${button_color}; color:${button_color};`);
+    windowUI.searchText.attr('style', `width:58vw; color:${button_color};`);
+
+    if (windowUI.otherStyles.cssRules.length > 0) {
+        windowUI.otherStyles.deleteRule(1);
+        windowUI.otherStyles.deleteRule(0);
     }
-    windowUI.other_styles.insertRule(`.select2-search__field::placeholder { color: ${button_color}; }`, 0);
-    windowUI.other_styles.insertRule(`.select2-selection__choice{ color: ${button_color} !important; }`, 1);
-    windowUI.search_text.attr('style',`width:58vw; color:${button_color};`);
+
+    windowUI.otherStyles.insertRule(`.select2-search__field::placeholder { color: ${button_color}; }`, 0);
+    windowUI.otherStyles.insertRule(`.select2-selection__choice{ color: ${button_color} !important; }`, 1);
+    windowUI.searchText.attr('style', `width:58vw; color:${button_color};`);
     windowUI.playlist.css('background-color', button_color);
-    windowUI.playlist.css('color', bg_color);
+    windowUI.playlist.css('color', bgColor);
 }
 
 function setDynamicBackground() {
-    var image = new Image;
+    const image = new Image;
     image.src = playerUI.thumbnail;
     get_handlers();
-    image.onload = function() {
-        var colorThief = new ColorThief();
-        var paletteArray = colorThief.getPalette(image, 2);
-        var dominantColor = paletteArray[0];
-        var otherColor = paletteArray[1];
-        windowUI.bg_color = `rgb(${dominantColor[0]}, ${dominantColor[1]}, ${dominantColor[2]})`;
-        windowUI.fg_color = `rgb(${otherColor[0]}, ${otherColor[1]}, ${otherColor[2]})`;
-        color_style(windowUI.fg_color, windowUI.bg_color);
+
+    image.onload = () => {
+        const colorThief = new ColorThief();
+        const paletteArray = colorThief.getPalette(image, 2);
+        const dominantColor = paletteArray[0];
+        const otherColor = paletteArray[1];
+        windowUI.bgColor = `rgb(${dominantColor[0]}, ${dominantColor[1]}, ${dominantColor[2]})`;
+        windowUI.fgColor = `rgb(${otherColor[0]}, ${otherColor[1]}, ${otherColor[2]})`;
+        color_style(windowUI.fgColor, windowUI.bgColor);
     }
 }
 
-window.onclick = function(event) {
+window.onclick = (event) => {
     if (event.target == loginModal) {
         loginModal.style.display = "none";
     }
@@ -257,8 +260,6 @@ function updateClient(serverState) {
         return;
     }
 
-    console.log(serverState);
-
     // Update Volume State
     player.volume = serverState.player.volume / 100;
     updateVolume();
@@ -267,26 +268,29 @@ function updateClient(serverState) {
     clearInterval(audioState.progressInterval);
 
     // Update Audio, playerUI, and Queue State
-    if(serverState.player.is_playing == true) {
+    if (serverState.player.is_playing == true) {
         audioState.song = serverState.player.current_track_info.title;
         audioState.id = serverState.player.current_track_info.id;
         audioState.time = serverState.player.current_track_info.current_time;
         audioState.endTime = serverState.player.current_track_info.duration;
         audioState.playedby = serverState.player.current_track_info.playedby
-        if(serverState.player.audio_status != "State.Paused"){
+
+        if (serverState.player.audio_status != "State.Paused") {
             audioState.progressInterval = setInterval(updateProgress, 1000);
         }
 
         if (playerUI.thumbnail != serverState.player.current_track_info.thumbnail_url) {
             playerUI.thumbnail = serverState.player.current_track_info.thumbnail_url;
-            playerUI.playerMain.css("background-image", `url(${playerUI.thumbnail})`);  
-            playerUI.playerMain.css("background-size", "cover"); 
+            playerUI.playerMain.css("background-image", `url(${playerUI.thumbnail})`);
+            playerUI.playerMain.css("background-size", "cover");
         }
+
         setDynamicBackground();
-        playerUI.progressBar.val(audioState.time/audioState.endTime);
+
+        playerUI.progressBar.val(audioState.time / audioState.endTime);
         playerUI.playerTitle.text(serverState.player.current_track_info.title);
 
-        if (serverState.queue != null){
+        if (serverState.queue != null) {
             reloadQueue(serverState.queue);
         }
     } else {
@@ -294,15 +298,19 @@ function updateClient(serverState) {
         audioState.time = 0;
         audioState.endTime = 0;
         audioState.playedby = null;
+
         playerUI.thumbnail = null;
-        playerUI.playerMain.css("background-image", "url(static/images/acm-logo.png)"); 
-        playerUI.playerMain.css("background-size", "100%"); 
+        playerUI.playerMain.css("background-image", "url(static/images/acm-logo.png)");
+        playerUI.playerMain.css("background-size", "100%");
         playerUI.progressBar.val(0);
         playerUI.playerTitle.text("ACM Concert");
+
         windowUI.body.css('background-color', 'rgba(34, 34, 34, 0.1)');
-        windowUI.bg_color = 'rgb(255, 255, 255)';
-        windowUI.fg_color = 'rgb(0, 0, 0)';
-        color_style(windowUI.fg_color, windowUI.bg_color); 
+        windowUI.bgColor = 'rgb(255, 255, 255)';
+        windowUI.fgColor = 'rgb(0, 0, 0)';
+
+        color_style(windowUI.fgColor, windowUI.bgColor);
+        
         queue.empty();
     }
 
@@ -314,6 +322,6 @@ function updateClient(serverState) {
     }
 }
 
-$('.trigger, .slider').click(function() {
+$('.trigger, .slider').click(() => {
     $('.slider').toggleClass('close');
-  });
+});
--- static/js/search.js
+++ static/js/search.js
@@ -1,80 +1,79 @@
-var searchJustActive = false;
-var userJustClicked = false;
-var searchCurrentlyActive = false;
+let searchJustActive = false;
+let userJustClicked = false;
+let searchCurrentlyActive = false;
 
-$(document).ready(function() {	
-	$("#url-textbox").select2({
-	  allowClear: false,
-	  multiple: true,
-      maximumSelectionSize: 1,
-	  ajax: {
-	    url: '/search',
-	    dataType: 'json',
-	    data: function (params) {
-	      return {
-	        q: params.term, // search term
-	        part: 'snippet',
-					maxResults: 15,
-					timeout: 1
-	      };
-	    },
-	    processResults: function (data, params) {
-	      // Filter by results with video ids
-	      //console.log(data.items);
-	      return {
-	        results: data.items
-	      };
-	    },
-	    cache: true
-	  },
-	  placeholder: 'Search for a song on Soundcloud or Youtube',
-	  escapeMarkup: function (markup) { return markup; }, // let our custom formatter work
-	  minimumInputLength: 1,
-	  templateResult: formatVideo,
-	  templateSelection: formatVideoSelection,
-	}).on("select2:select", function() {
-		if (!userJustClicked) {
-			searchJustActive = true;
-		}
-		document.activeElement.blur();
-	}).on("select2:opening", function() {
-		$("#url-textbox").val(null).trigger('change');
-		searchCurrentlyActive = true;
-	}).on("select2:close", function() {
-		searchCurrentlyActive = false;
-	});
+$(document).ready(() => {
+    $("#url-textbox").select2({
+        allowClear: false,
+        multiple: true,
+        maximumSelectionSize: 1,
+        ajax: {
+            url: '/search',
+            dataType: 'json',
+            data: (params) => {
+                return {
+                    q: params.term, // search term
+                    part: 'snippet',
+                    maxResults: 15,
+                    timeout: 1
+                };
+            },
+            processResults: (data, params) => {
+                // Filter by results with video ids
+                return { results: data.items };
+            },
+            cache: true
+        },
+        placeholder: 'Search for a song on Soundcloud or Youtube',
+        escapeMarkup: markup => markup, // let our custom formatter work
+        minimumInputLength: 1,
+        templateResult: formatVideo,
+        templateSelection: formatVideoSelection,
+    }).on("select2:select", () => {
+        if (!userJustClicked) {
+            searchJustActive = true;
+        }
 
-	if (!loggedin) {
-		$("#url-textbox").select2('enable', false);
-		$("#url-textbox").select2({
-			placeholder: 'Please login to add to the queue'
-		});
-	}
+        document.activeElement.blur();
+    }).on("select2:opening", () => {
+        $("#url-textbox").val(null).trigger('change');
+        searchCurrentlyActive = true;
+    }).on("select2:close", () => {
+        searchCurrentlyActive = false;
+    });
+
+    if (!isUserLoggedIn) {
+        $("#url-textbox").select2('enable', false);
+        $("#url-textbox").select2({
+            placeholder: 'Please login to add to the queue'
+        });
+    }
 });
 
-function formatVideo (video) {
-	if (video.loading) {
-		return video.text;
-	}
+function formatVideo(video) {
+    if (video.loading) {
+        return video.text;
+    }
 
-	var markup = 
- 	"<div class='select2-result-repository clearfix'>" +
-	"<div class='select2-result-repository__avatar'><img src='" + video.snippet.thumbnails.high.url + "' /></div>" +
-	"<div class='select2-result-repository__meta'>" + 
-	"<div class='select2-result-repository__title'>" + video.snippet.title + "</div>" +
-	"<div class='select2-result-repository__description'>" + video.trackType + "</div></div>";
+    const markup =
+        "<div class='select2-result-repository clearfix'>" +
+        "<div class='select2-result-repository__avatar'><img src='" + video.snippet.thumbnails.high.url + "' /></div>" +
+        "<div class='select2-result-repository__meta'>" +
+        "<div class='select2-result-repository__title'>" + video.snippet.title + "</div>" +
+        "<div class='select2-result-repository__description'>" + video.trackType + "</div></div>";
 
-	var resultItem = $(markup);
-	resultItem.on('mouseup', function(evt) {
-		userJustClicked = true;
-	});
+    const resultItem = $(markup);
+    resultItem.on('mouseup', (evt) => {
+        userJustClicked = true;
+    });
 
-	return resultItem;
+    return resultItem;
 }
 
-function formatVideoSelection (video) {
-	if (video.trackType == "SoundCloud") {
-		return video.snippet.url;
-	}
-	return "https://www.youtube.com/watch?v=" + video.id;
+function formatVideoSelection(video) {
+    if (video.trackType == "SoundCloud") {
+        return video.snippet.url;
+    }
+
+    return "https://www.youtube.com/watch?v=" + video.id;
 }
