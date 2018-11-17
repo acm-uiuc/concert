9417b96bade75cd430d14d688805b38436a2df45
asherdale <asher@dales.org>
2018-11-17 15:11:12 -0600

    Add searchbar with auto-suggestion

--- src/main.js
+++ src/main.js
@@ -1,23 +1,12 @@
 import React, { Component } from "react";
 import "./Main.scss";
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import SearchBar from 'material-ui-search-bar';
+import SearchBar from "./SearchBar"
 
 export default class Main extends Component {
   render() {
     return (
       <div id="main-root">
-        <MuiThemeProvider>
-          <SearchBar
-            onChange={() => console.log('onChange')}
-            onRequestSearch={() => console.log('onRequestSearch')}
-            style={{
-              margin: 'auto',
-              maxWidth: 800,
-              flexGrow: 1
-            }}
-          />
-        </MuiThemeProvider>
+        <SearchBar />
       </div>
     );
   }
