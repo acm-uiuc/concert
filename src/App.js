90630338727471e81afb85bd13819f7e0dcf9c02
asherdale <asher@dales.org>
2018-11-17 09:42:56 -0600

    remove unneeded files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
 import "./App.css";
-import Main from "./main.js";
+import Main from "./Main.js";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Main name="Asher"/>
+        
         <div class="sidenav">
           <a href="#about">Link 1</a>
           <a href="#services">Link 2</a>
           <a href="#clients">Link 3</a>
           <a href="#contact">Link 4</a>
         </div>
+
         <div className="StatusBar">
           <h1>"This is text."</h1>
         </div>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-import React, { Component } from "react";
+import React from 'react';
 
 export default class Main extends React.Component {
   render() {
-    return <h1>Hello, {this.props.name}</h1>;
+    return (
+      <div>
+        <h1>Hello, {this.props.name}</h1>
+      </div>
+    );
   }
 }
