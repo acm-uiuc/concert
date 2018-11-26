978e3e47dcd8f406720fa0a2b7cb826728ed6567
asherdale <asher@dales.org>
2018-11-26 17:31:40 -0600

    Decompose elements

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React, { Component } from "react";
 import "./App.scss";
 import Main from "./Main";
+import SideBar from "./SideBar";
+import StatusBar from "./StatusBar";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <div className="sidenav">
-          <a href="#about">Link 1</a>
-          <a href="#services">Link 2</a>
-          <a href="#clients">Link 3</a>
-          <a href="#contact">Link 4</a>
-        </div>
-
+        <SideBar />
         <Main />
-
-        <div className="StatusBar">
-          <h1>"This is text."</h1>
-        </div>
+        <StatusBar />
       </div>
     );
   }
---
+++ src/StatusBar.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./StatusBar.scss";
+
+export default class StatusBar extends Component {
+  render() {
+    return (
+        <div className="StatusBar">
+          <h1>"This is text."</h1>
+        </div>
+    );
+  }
+}
