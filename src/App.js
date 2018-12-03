d50a7f8f0f149da058ae10ffeeb6694ffd4ca637
km2018 <karthik.murthy123@gmail.com>
2018-12-03 13:45:53 -0600

    Add linear gradient

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ class App extends Component {
   render() {
     return (
       <div>
+
         <div class="sidenav">
-          <a href="#about">Link 1</a>
-          <a href="#services">Link 2</a>
-          <a href="#clients">Link 3</a>
-          <a href="#contact">Link 4</a>
+        	<div class = "sidecontainer">
+          	<a href="#about">Link 1</a>
+          	<a href="#services">Link 2</a>
+          	<a href="#clients">Link 3</a>
+          	<a href="#contact">Link 4</a>
+         	 </div>
         </div>
         <div className="StatusBar">
           <h1>"This is text."</h1>
