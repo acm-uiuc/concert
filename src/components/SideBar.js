792d336bd77abfdde4716c7ad233d79d064c9409
Ishaan Sehgal <ishaan.sehgal99@gmail.com>
2018-12-04 15:51:49 -0600

    Added sexy gradient and changed margins

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -4,12 +4,14 @@ import "./SideBar.scss";
 export default class SideBar extends Component {
   render() {
     return (
-        <div className="sidenav">
-            <a href="#about">Link 1</a>
-            <a href="#services">Link 2</a>
-            <a href="#clients">Link 3</a>
-            <a href="#contact">Link 4</a>
+      <div className="sidenav">
+        <div className="links">
+          <a href="#about">Link 1</a>
+          <a href="#services">Link 2</a>
+          <a href="#clients">Link 3</a>
+          <a href="#contact">Link 4</a>
         </div>
+      </div>
     );
   }
 }
